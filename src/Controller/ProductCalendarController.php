3bb00103544f34cf80fa93458ab05eb90de016d6
velzimar <hjoussema@gmail.com>
2021-03-14 22:47:34 +0100

    calendar more

--- src/Controller/ProductCalendarController.php
+++ src/Controller/ProductCalendarController.php
@@ -2,31 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Entity\ProductCalendar;
 use App\Form\ProductCalendarType;
+use DateTime;
+use Doctrine\DBAL\Types\DateTimeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Time;
 
 class ProductCalendarController extends AbstractController
 {
     /**
-     * @Route("/product/calendar", name="product_calendar")
+     * @Route("/product/{id}/calendar", name="product_calendar")
      * @param Request $request
+     * @param Product $product
      * @return Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request, Product $product): Response
     {
+
         $productCalendar = new ProductCalendar();
         $form = $this->createForm(ProductCalendarType::class, $productCalendar);
 
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $productCalendar->setProduct($product);
             $productCalendar->setSlots(json_decode($form->get('slots')->getData()));
+            $period_string = $form->get('period')->getData().":00";
+
+            dump($period_string);
+            $format = 'H:i:s';
+            $date = DateTime::createFromFormat($format, $period_string);
+            dump($date);
             dump($productCalendar);
             die();
+            $productCalendar->setPeriod();
             $em = $this->getDoctrine()->getManager();
             $em->persist($productCalendar);
             $em->flush();
--- src/Form/ProductCalendarType.php
+++ src/Form/ProductCalendarType.php
@@ -4,8 +4,13 @@ namespace App\Form;
 
 use App\Entity\ProductCalendar;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductCalendarType extends AbstractType
@@ -13,14 +18,54 @@ class ProductCalendarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('period')
-            ->add('start')
-            ->add('end')
             ->add('isActive')
-            ->add('slots',TextareaType::class,[
-                "mapped"=>false,
+            ->add('period', TextType::class, [
+                "mapped" => false,
+            ]) ->add('start', DateTimeType::class, [
+                'label' => "Date d'activation",
+                'required'=>false,
+                'widget' => 'single_text',
             ])
-        ;
+            ->add('end', DateTimeType::class, [
+                'label' => "Dernièr jour ouvert pour réservation",'required'=>false,
+                'widget' => 'single_text',
+            ])
+            ->add('slots', TextareaType::class, [
+                "mapped" => false,
+            ]);
+
+        /*
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $user = $event->getData();
+            $form = $event->getForm();
+
+            if (!$user) {
+                return;
+            }
+
+            // checks whether the user has chosen to display their email or not.
+            // If the data was submitted previously, the additional value that is
+            // included in the request variables needs to be removed.
+            if (isset($user['isActive']) && $user['isActive']) {
+                $form
+                    ->add('start', DateTimeType::class, [
+                    'label' => "Date d'activation",
+                    'required' => true,
+                    'widget' => 'single_text',
+                    ])
+                    ->add('end', DateTimeType::class, [
+                        'required' => true,
+                        'label' => "Dernièr jour ouvert pour réservation",
+                        'widget' => 'single_text',
+                    ]);
+            } else {
+                unset($user['start']);
+                unset($user['end']);
+                $event->setData($user);
+            }
+        });
+
+        */
     }
 
     public function configureOptions(OptionsResolver $resolver)
