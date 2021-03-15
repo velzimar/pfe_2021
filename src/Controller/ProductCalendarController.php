bd7a43687a1244f2c68d50a2d2f455d6c769d4a4
velzimar <hjoussema@gmail.com>
2021-03-15 20:55:21 +0100

    fin reservation

--- src/Controller/ProductCalendarController.php
+++ src/Controller/ProductCalendarController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Time;
 /**
- * @Route("/product")
+ * @Route("/productCalendar")
  * @method User|null getUser()
  */
 class ProductCalendarController extends AbstractController
@@ -23,7 +23,7 @@ class ProductCalendarController extends AbstractController
 
     //USER
     /**
-     * @Route("/{id}/calendar", name="product_calendar")
+     * @Route("/user/product/{id}/", name="product_calendar")
      * @param Request $request
      * @param Product $product
      * @param ProductCalendarRepository $rep
@@ -48,7 +48,7 @@ class ProductCalendarController extends AbstractController
             }
 
 
-            return $this->render('product_calendar/edit.html.twig', [
+            return $this->render('product_calendar/user/edit.html.twig', [
                 'controller_name' => 'ProductCalendarController',
                 'form' => $form->createView(),
                 'product'=>$product,
@@ -71,7 +71,7 @@ class ProductCalendarController extends AbstractController
             }
 
 
-            return $this->render('product_calendar/index.html.twig', [
+            return $this->render('product_calendar/user/index.html.twig', [
                 'controller_name' => 'ProductCalendarController',
                 'form' => $form->createView(),
                 'product'=>$product
@@ -79,5 +79,72 @@ class ProductCalendarController extends AbstractController
         }
 
     }
+    //admin
+
+    /**
+     * @Route("/admin/user/{user}/product/{id}/", name="admin_product_calendar")
+     * @param Request $request
+     * @param User $user
+     * @param Product $product
+     * @param ProductCalendarRepository $rep
+     * @return Response
+     */
+    public function indexAdmin(Request $request, User $user, Product $product, ProductCalendarRepository $rep): Response
+    {
+        $res = $rep->findOneBy(["product"=>$product]);
+        //dump($res);die();
+        if($res!==null){
+            $productCalendar = $res;
+            $form = $this->createForm(ProductCalendarType::class, $productCalendar);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $productCalendar->setProduct($product);
+                $productCalendar->setSlots(json_decode($form->get('slots')->getData()));
+                //  dump($productCalendar);die();
+                $em = $this->getDoctrine()->getManager();
+                //$em->persist($productCalendar);
+                $em->flush();
+                return $this->redirectToRoute("product_index_user",[
+                    "userId"=>$user
+                ]);
+            }
+
+
+            return $this->render('product_calendar/edit.html.twig', [
+                'controller_name' => 'ProductCalendarController',
+                'form' => $form->createView(),
+                'product'=>$product,
+                'slots'=>$productCalendar->getSlots(),
+                'user'=>$user
+            ]);
+        }else{
+            $productCalendar = new ProductCalendar();
+            $form = $this->createForm(ProductCalendarType::class, $productCalendar);
+
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $productCalendar->setProduct($product);
+                $productCalendar->setSlots(json_decode($form->get('slots')->getData()));
+                //  dump($productCalendar);die();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($productCalendar);
+                $em->flush();
+                return $this->redirectToRoute("product_index_user",[
+                    "userId"=>$user
+                ]);
+            }
+
+
+            return $this->render('product_calendar/index.html.twig', [
+                'controller_name' => 'ProductCalendarController',
+                'form' => $form->createView(),
+                'product'=>$product,
+                'user'=>$user
+            ]);
+        }
+
+    }
+
 }
 
