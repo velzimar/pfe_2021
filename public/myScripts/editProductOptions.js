3fa1256fe4d78e584de87b189359e74d20bad07d
velzimar <hjoussema@gmail.com>
2021-03-18 20:04:37 +0100

    product option add prices and edit script and interface

--- public/myScripts/editProductOptions.js
+++ public/myScripts/editProductOptions.js
@@ -1,3 +1,4 @@
+/*
 var all_choices = [];
 
 //var current_length = 0
@@ -42,48 +43,15 @@ function _add() {
         }
     }
     op = "<option selected value='"+j.value+"' data-price='"+price.value+"' data-name='"+j.value+"' >"+j.value + " à "+price.value+ " dt"+"</option>"
-    /*
-    var option = document.createElement("option")
-    option.value = j.value;
-    option.text = j.value + " à "+price.value+ " dt";
-    option.selected = true;
-    option.data_price=price.value*/
+
     $("#product_options_choices").append(op);
 
 
-    /*
-    $("#product_options_choices").append($('<option />')  // Create new <option> element
-            .val(j.value)            // Set value as "Hello"
-            .text(j.value + " à "+price.value+ " dt")           // Set textContent as "Hello"
-            .prop('selected', true)  // Mark it selected
-            .data({                  // Set multiple data-* attributes
-                price: price.value,
-                name:j.value
-            })
-        );
-    */
     $("#input_for_choices").val("");
     price.value = null;
     setCurrent_length(current_length + 1)
 }
 
-
-function _delete() {
-    var x = document.getElementById("product_options_choices");
-    x.remove(x.selectedIndex);
-    setCurrent_length(current_length - 1)
-}
-
-function _check_json_content() {
-    if (all_choices.length === 0) alert("check => json is empty")
-    var jsoncheck = []
-    for (var k = 0; k < all_choices.length; k++) {
-        jsoncheck.push(JSON.stringify(all_choices[k]))
-    }
-    alert("check => " + jsoncheck)
-}
-
-const button = document.getElementById('post-btn');
 
 
 $("#post-btn").click(function (e) {
@@ -176,4 +144,175 @@ $("#post-btn").click(function (e) {
     });
 
 
+});
+
+*/
+
+//from user
+/*
+let price_id = "input_for_prices"
+let choice_input_native_id = "input_for_choices"
+let choices_native_id = "product_options_choices"
+let choice_number_native_id = "nbChoix"
+let name_id = "nom"
+let ajax_post_url = "/productOptions/list"
+let ajax_post_type = "POST"
+let post_button_native_id = "post-btn"
+*/
+//from script
+
+let all_choices = [];
+
+let price_native = document.getElementById(price_id);
+
+let choice_input_jquery_id = "#" + choice_input_native_id
+let choice_input_native = document.getElementById(choice_input_native_id);
+let choice_input_jquery = $(choice_input_jquery_id)
+
+let choices_jquery_id = "#" + choices_native_id
+let choices_native = document.getElementById(choices_native_id);
+let choices_jquery = $(choices_jquery_id);
+
+let name_input_native = document.getElementById(name_id)
+
+let choice_number_jquery_id = "#" + choice_number_native_id
+let choice_number_jquery = $(choice_number_jquery_id)
+
+let post_button_jquery_id = "#" + post_button_native_id
+let jquery_post_button = $(post_button_jquery_id)
+
+
+function setCurrent_length(y, selectedElement) {
+    //alert("selectedelement " + selectedElement)
+    choice_number_jquery.children().remove().end()
+    current_length = y;
+    for (let i = 1; i <= y; i++) {
+        if (i.toString() === selectedElement) {
+            // alert("i= " + i + " selected: " + selectedElement)
+            choice_number_jquery.append($("<option selected    />").val(i).text(i));
+        } else
+            choice_number_jquery.append($("<option     />").val(i).text(i));
+    }
+    //alert(current_length)
+}
+
+function _add() {
+    if (price_native.value === '' || parseFloat(price_native.value) <= 0) {
+        alert("Vérifier le prix");
+        return;
+    }
+
+    if (choice_input_native.value === null || choice_input_native.value === "" || choice_input_native.value.trim() === "") {
+        alert("Inserer le choix")
+        choice_input_native.value = null;
+        return;
+    }
+
+    for (i = 0; i < choices_native.length; ++i) {
+        //alert(choices_native.options[i].value)
+        if (choices_native.options[i].value === choice_input_native.value.trim()) {
+            alert("Choix existe déja");
+            choice_input_native.value = null;
+            return;
+        }
+    }
+
+    choices_jquery.append("<option selected value='" + choice_input_native.value.trim() + "' data-price='" + parseFloat(price_native.value) + "' data-name='" + choice_input_native.value.trim() + "' >" + choice_input_native.value.trim() + " à " + parseFloat(price_native.value) + " dt" + "</option>");
+    choice_input_jquery.val("");
+    price_native.value = null;
+    setCurrent_length(current_length + 1)
+}
+
+function _delete() {
+    choices_native.remove(choices_native.selectedIndex);
+    setCurrent_length(current_length - 1)
+}
+
+function _check_json_content() {
+    if (all_choices.length === 0) alert("check => json is empty")
+    let jsoncheck = []
+    for (let k = 0; k < all_choices.length; k++) {
+        jsoncheck.push(JSON.stringify(all_choices[k]))
+    }
+    alert("check => " + jsoncheck)
+}
+
+
+jquery_post_button.click(function (e) {
+    e.preventDefault();
+    if (name_input_native.value === null || name_input_native.value === "" || name_input_native.value.trim() === "") {
+        alert("Inserer le nom de l'option")
+        name_input_native.value = null;
+        return;
+    }
+    for (let f = 0; f < all_choices.length; f++) {
+        if (all_choices[f]['nom'] === name_input_native.value.trim()) {
+            alert("Le nom de l'option doit être unique");
+            name_input_native.value = null;
+            return
+        }
+    }
+
+    for (let k = 0; k < option_names.length; k++) {
+        if (option_names[k] === name_input_native.value.trim()) {
+            alert("Le nom du l'option doit être unique");
+            name_input_native.value = null;
+            return
+        }
+    }
+
+    let selectedNbChoices = $(choice_number_jquery_id + " option:selected").val();
+    let json_obj = {};
+    let results = [];
+    let es = $(choices_jquery_id + " option");
+    $.map(es, e => {
+        alert($(e).val());
+        alert($(e).data('price'))
+        results.push({name: $(e).data('name'), price: $(e).data('price')});
+    })
+
+    //alert(results);
+    if (results.length > 0) {
+        json_obj = {
+            nom: name_input_native.value.trim(),
+            choices: results,
+            selectedNbChoices: selectedNbChoices,
+            product: product_id
+        };
+        all_choices.push(json_obj);
+    } else {
+        alert("Remplir les choix");
+        return;
+    }
+
+    let data_to_send
+    if(all_choices.length === 0)  all_choices = []
+
+    if (typeof user_id === 'undefined' && typeof option_id === 'undefined')
+        data_to_send = {array: all_choices, product_id: product_id}
+    else if (typeof user_id !== 'undefined' && typeof option_id === 'undefined')
+        data_to_send = {array: all_choices, product_id: product_id, user_id: user_id}
+    else if (typeof user_id === 'undefined' && typeof option_id !== 'undefined')
+        data_to_send = {array: all_choices, product_id: product_id, option_id: parseInt(option_id) }
+    else
+        data_to_send = {array: all_choices, product_id: product_id, user_id: user_id , option_id: parseInt(option_id)}
+
+    $.ajax({
+        type: ajax_post_type,
+        url: ajax_post_url,//post how you get this URL please...
+        data: data_to_send,//jQ will sort this out for you
+        success: function (response) {
+            console.log('aaaaa');
+            if (response.success) {
+                console.log('sucess');
+                window.location.href = response.redirect; // <-- HERE
+            }
+        },
+        error: function (response) {
+            console.log('an error occured');
+            if (!response.success) {
+                console.log('fail');
+            }
+        }
+    });
 });
--- public/myScripts/productOptions.js
+++ public/myScripts/productOptions.js
@@ -1,202 +1,165 @@
-var all_choices = [];
+//from user
+/*
+let price_id = "input_for_prices"
+let choice_input_native_id = "input_for_choices"
+let choices_native_id = "product_options_choices"
+let choice_number_native_id = "nbChoix"
+let name_id = "nom"
+let ajax_post_url = "/productOptions/list"
+let ajax_post_type = "POST"
+let post_button_native_id = "post-btn"
+*/
+//from script
+
+let all_choices = [];
 let current_length = 0
+
+let price_native = document.getElementById(price_id);
+
+let choice_input_jquery_id = "#" + choice_input_native_id
+let choice_input_native = document.getElementById(choice_input_native_id);
+let choice_input_jquery = $(choice_input_jquery_id)
+
+let choices_jquery_id = "#" + choices_native_id
+let choices_native = document.getElementById(choices_native_id);
+let choices_jquery = $(choices_jquery_id);
+
+let name_input_native = document.getElementById(name_id)
+
+let choice_number_jquery_id = "#" + choice_number_native_id
+let choice_number_jquery = $(choice_number_jquery_id)
+
+let post_button_jquery_id = "#" + post_button_native_id
+let jquery_post_button = $(post_button_jquery_id)
+
+let container_jquery_id = "#"+container_id;
+let container_jquery = $(container_jquery_id)
 
 function setCurrent_length(y, selectedElement) {
     //alert("selectedelement " + selectedElement)
-    let list = $("#nbChoix");
-    list.children().remove().end()
+    choice_number_jquery.children().remove().end()
     current_length = y;
     for (let i = 1; i <= y; i++) {
         if (i.toString() === selectedElement) {
-           // alert("i= " + i + " selected: " + selectedElement)
-
-            list.append($("<option selected    />").val(i).text(i));
+            // alert("i= " + i + " selected: " + selectedElement)
+            choice_number_jquery.append($("<option selected    />").val(i).text(i));
         } else
-            list.append($("<option     />").val(i).text(i));
+            choice_number_jquery.append($("<option     />").val(i).text(i));
     }
     //alert(current_length)
 }
 
 function _add() {
-    //added for price
-    var price = document.getElementById("input_for_prices");
-    if(price.value === '' || price.value === 0){
+    if (price_native.value === '' || parseFloat(price_native.value) <= 0) {
         alert("Vérifier le prix");
         return;
     }
 
-
-    var x = document.getElementById("product_options_choices");
-    var j = document.getElementById("input_for_choices");
-
-    if (j.value === null || j.value === "" || j.value.trim() === "") {
+    if (choice_input_native.value === null || choice_input_native.value === "" || choice_input_native.value.trim() === "") {
         alert("Inserer le choix")
-        j.value = null;
+        choice_input_native.value = null;
         return;
     }
-    for (i = 0; i < x.length; ++i) {
-        //alert(x.options[i].value)
-        if (x.options[i].value === j.value) {
+
+    for (i = 0; i < choices_native.length; ++i) {
+        //alert(choices_native.options[i].value)
+        if (choices_native.options[i].value === choice_input_native.value.trim()) {
             alert("Choix existe déja");
-            j.value = null;
+            choice_input_native.value = null;
             return;
         }
     }
-    op = "<option selected value='"+j.value+"' data-price='"+price.value+"' data-name='"+j.value+"' >"+j.value + " à "+price.value+ " dt"+"</option>"
-    /*
-    var option = document.createElement("option")
-    option.value = j.value;
-    option.text = j.value + " à "+price.value+ " dt";
-    option.selected = true;
-    option.data_price=price.value*/
-    $("#product_options_choices").append(op);
 
-
-    /*
-    $("#product_options_choices").append($('<option />')  // Create new <option> element
-            .val(j.value)            // Set value as "Hello"
-            .text(j.value + " à "+price.value+ " dt")           // Set textContent as "Hello"
-            .prop('selected', true)  // Mark it selected
-            .data({                  // Set multiple data-* attributes
-                price: price.value,
-                name:j.value
-            })
-        );
-    */
-$("#input_for_choices").val("");
-    price.value = null;
+    choices_jquery.append("<option selected value='" + choice_input_native.value.trim() + "' data-price='" + parseFloat(price_native.value) + "' data-name='" + choice_input_native.value.trim() + "' >" + choice_input_native.value.trim() + " à " + parseFloat(price_native.value) + " dt" + "</option>");
+    choice_input_jquery.val("");
+    price_native.value = null;
     setCurrent_length(current_length + 1)
 }
 
 function _save() {
-    var name = document.getElementById("nom").value;
-    if (name === null || name === "" || name.trim() === "") {
-        alert("Inserer le nom du l'option")
-        document.getElementById("nom").value = null;
+    if (name_input_native.value === null || name_input_native.value === "" || name_input_native.value.trim() === "") {
+        alert("Inserer le nom de l'option")
+        name_input_native.value = null;
         return;
     }
-    for (var f = 0; f < all_choices.length; f++) {
-        if (all_choices[f]['nom'] === name) {
-            alert("Le nom du l'option doit être unique");
-            document.getElementById("nom").value = null;
+    for (let f = 0; f < all_choices.length; f++) {
+        if (all_choices[f]['nom'] === name_input_native.value.trim()) {
+            alert("Le nom de l'option doit être unique");
+            name_input_native.value = null;
             return
         }
     }
 
-    for (var k = 0; k < option_names.length; k++) {
-        if (option_names[k] === name) {
+    for (let k = 0; k < option_names.length; k++) {
+        if (option_names[k] === name_input_native.value.trim()) {
             alert("Le nom du l'option doit être unique");
-            document.getElementById("nom").value = null;
+            name_input_native.value = null;
             return
         }
     }
-    var elements = document.getElementById("product_options_choices");
 
-/*
-   var e = $("#product_options_choices").each(function()
-     {
-         alert($(this).html())
-         alert($(this).getAttribute('data-price'))
-     });
+    let selectedNbChoices = $(choice_number_jquery_id + " option:selected").val();
+    let json_obj = {};
+    let results = [];
+    let es = $(choices_jquery_id + " option");
+    $.map(es, e => {
+        alert($(e).val());
+        alert($(e).data('price'))
+        results.push({name: $(e).data('name'), price: $(e).data('price')});
+    })
 
-  */
-    let selectedNbChoices = $("#nbChoix option:selected").val();
-    var json_obj = {};
-    var results = [];
-    var es = $("#product_options_choices option");
-    var v = $.map(es, e => {
-        alert($(e).val());alert($(e).data('price'))
-
-        results.push({name:$(e).data('name'),price:$(e).data('price')});
-    }  )
-
-    /*
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        //var strSel = element.options[element.selectedIndex].text;
-        results.push(element.text);
-    }
-    */
     //alert(results);
     if (results.length > 0) {
-        var nom = document.getElementById("nom");
-        json_obj = {nom: nom.value, choices: results, selectedNbChoices: selectedNbChoices, product:product_id};
+        json_obj = {
+            nom: name_input_native.value.trim(),
+            choices: results,
+            selectedNbChoices: selectedNbChoices,
+            product: product_id
+        };
         all_choices.push(json_obj);
 
-
-        var choicesElement = "<div class='element-box' id='row_for_" + nom.value + "' ><div class='form-desc'><table class='table'><tbody><tr><th>Nom du l'option</th><td>" + nom.value + "</td></tr><tr><th>Les choix</th><td><ul id='choices_for_azeae'>"
-        results.forEach(option => choicesElement += "<li style='width:100%;word-break:break-all;overflow-wrap: break-word;height: fit-content;'><h5 style='color: #ccd9e8;'><h6>" + option["name"] +" à "+ option["price"]  + "</h6></li>");
-        choicesElement += "</ul></td></tr><tr><th>Nombre maximum de choix à selectionner</th><td>" + selectedNbChoices + "</td></tr><tr><th>Actions</th><td><div>"
-        choicesElement += "<button class='btn btn-primary' data-num='' name='" + nom.value + "' id='Edit_" + nom.value + "' type='button' '>Modifier</button><button class='btn btn-danger' data-num='' id='Delete_" + nom.value + "' type='button' '>Supprimer</button></div></td></tr></tbody></table></div></div>"
-        $("#newOptions_container").append(choicesElement);
-
-
-        editElement = $("#Edit_" + nom.value)
-        editElement.data("num", json_obj);
-        editElement.on("click", function () {
-            var jsonObj = $(this).data("num");
-            var choicesElement = document.getElementById("product_options_choices")
-            var choicesInputElement = document.getElementById("input_for_choices")
-            var nomElement = document.getElementById("nom")
-            //clearing old choices
-            $('#product_options_choices').children().remove().end()
-            nomElement.value = null;
-            choicesInputElement.value = null;
-            //adding the selected object choices to the element for edit
-            //alert("heeeeere" + jsonObj["selectedNbChoices"])
-            setCurrent_length(jsonObj["choices"].length, jsonObj["selectedNbChoices"])
-            for (var x = 0; x < jsonObj["choices"].length; x++) {
-
-                op = "<option selected value='"+jsonObj['choices'][x]['name']+"' data-price='"+jsonObj['choices'][x]['price']+"' data-name='"+jsonObj['choices'][x]['name']+"' >"+jsonObj['choices'][x]['name'] + " à "+jsonObj['choices'][x]['price']+ " dt"+"</option>"
-
-                $("#product_options_choices").append(op);
-/*
-                var opt = document.createElement('option');
-                opt.appendChild(document.createTextNode(jsonObj["choices"][x]));
-                opt.value = jsonObj["choices"][x];
-                opt.selected = true;
-                choicesElement.appendChild(opt);
-  */
-
-
-            }
-            //adding the selected object name for edit
-            nomElement.value = jsonObj["nom"];
+        function delete_from_json(obj) {
             //delete from json
-            for (var t = 0; t < all_choices.length; t++) {
+            for (let t = 0; t < all_choices.length; t++) {
                 //alert($(this).data("num")["nom"])
-                if (all_choices[t]["nom"] === $(this).data("num")["nom"]) {
+                if (all_choices[t]["nom"] === obj.data("num")["nom"]) {
                     all_choices.splice(t, 1)
                     t--
                 }
             }
+        }
+
+        let choicesElement = "<div class='element-box' id='row_for_" + name_input_native.value + "' ><div class='form-desc'><table class='table'><tbody><tr><th>Nom du l'option</th><td>" + name_input_native.value + "</td></tr><tr><th>Les choix</th><td><ul id='choices_for_azeae'>"
+        results.forEach(option => choicesElement += "<li style='width:100%;word-break:break-all;overflow-wrap: break-word;height: fit-content;'><h5 style='color: #ccd9e8;'><h6>" + option["name"] + " à " + option["price"] + " dt</h6></li>");
+        choicesElement += "</ul></td></tr><tr><th>Nombre maximum de choix à selectionner</th><td>" + selectedNbChoices + "</td></tr><tr><th>Actions</th><td><div>"
+        choicesElement += "<button class='btn btn-primary' data-num='' name='" + name_input_native.value + "' id='Edit_" + name_input_native.value + "' type='button' '>Modifier</button><button class='btn btn-danger' data-num='' id='Delete_" + name_input_native.value + "' type='button' '>Supprimer</button></div></td></tr></tbody></table></div></div>"
+        container_jquery.append(choicesElement);
+
+        let editElement = $("#Edit_" + name_input_native.value)
+        editElement.data("num", json_obj);
+        editElement.on("click", function () {
+            let jsonObj = $(this).data("num");
+            //clearing old choices
+            choices_jquery.children().remove().end()
+            name_input_native.value = null;
+            choice_input_native.value = null;
+            //adding the selected object choices to the element for edit
+            setCurrent_length(jsonObj["choices"].length, jsonObj["selectedNbChoices"])
+            for (let inc = 0; inc < jsonObj["choices"].length; inc++) {
+                choices_jquery.append("<option selected value='" + jsonObj['choices'][inc]['name'] + "' data-price='" + jsonObj['choices'][inc]['price'] + "' data-name='" + jsonObj['choices'][inc]['name'] + "' >" + jsonObj['choices'][inc]['name'] + " à " + jsonObj['choices'][inc]['price'] + " dt" + "</option>");
+            }
+            //adding the selected object name for edit
+            name_input_native.value = jsonObj["nom"];
+            delete_from_json($(this))
             document.getElementById("row_for_" + $(this).data('num')['nom']).remove();
         });
 
 
-        editElement = $("#Delete_" + nom.value)
-        editElement.data("num", json_obj);
-        editElement.on("click", function () {
+        let deleteElement = $("#Delete_" + name_input_native.value)
+        deleteElement.data("num", json_obj);
+        deleteElement.on("click", function () {
             //alert("the element is"+JSON.stringify($(this).data("num")))
-            /*
-            for (var k = 0; k < all_choices.length; k++) {
-                alert("current table"+JSON.stringify(all_choices[k]))
-            }
-            */
-            //delete from json
-            for (var t = 0; t < all_choices.length; t++) {
-                //alert($(this).data("num")["nom"])
-                if (all_choices[t]["nom"] === $(this).data("num")["nom"]) {
-                    all_choices.splice(t, 1)
-                    t--
-                }
-            }
-            /*
-            for (var k2 = 0; k2 < all_choices.length; k2++) {
-                alert("after delete table"+JSON.stringify(all_choices[k2]))
-            }
-
-             */
+            delete_from_json($(this))
             //delete from view
             document.getElementById("row_for_" + $(this).data('num')['nom']).remove();
         });
@@ -205,41 +168,65 @@ function _save() {
         alert("Remplir les choix");
         return
     }
-    $('#product_options_choices').children().remove().end()
-    document.getElementById("input_for_choices").value = null;
-    document.getElementById("nom").value = null;
-
+    choices_jquery.children().remove().end()
+    choice_input_native.value = null;
+    name_input_native.value = null;
     setCurrent_length(0)
 }
 
 function _delete() {
-    var x = document.getElementById("product_options_choices");
-    x.remove(x.selectedIndex);
+    choices_native.remove(choices_native.selectedIndex);
     setCurrent_length(current_length - 1)
 }
 
 function _check_json_content() {
     if (all_choices.length === 0) alert("check => json is empty")
-    var jsoncheck = []
-    for (var k = 0; k < all_choices.length; k++) {
+    let jsoncheck = []
+    for (let k = 0; k < all_choices.length; k++) {
         jsoncheck.push(JSON.stringify(all_choices[k]))
     }
     alert("check => " + jsoncheck)
 }
 
-const button = document.getElementById('post-btn');
 
-$("#post-btn").click(function (e) {
-
-    alert("am clicked")
-    if(all_choices.length === 0){
+jquery_post_button.click(function (e) {
+    e.preventDefault();
+    let data_to_send
+    if (all_choices.length === 0) {
+        if (typeof user_id === 'undefined')
+            data_to_send = {array: [], product_id: product_id}
+        else
+            data_to_send = {array: [], product_id: product_id, user_id: user_id}
 
         $.ajax({
-            type: "POST",
-            url: "/productOptions/list",//post how you get this URL please...
-            data: {array: [], product_id: product_id},//jQ will sort this out for you
+            type: ajax_post_type,
+            url: ajax_post_url,//post how you get this URL please...
+            data: data_to_send,//jQ will sort this out for you
             success: function (response) {
                 console.log('empty');
+                if (response.success) {
+                    console.log('success');
+                    window.location.href = response.redirect; // <-- HERE
+                }
+            },
+            error: function (response) {
+                console.log('an error occured');
+                if (!response.success) {
+                    console.log('fail');
+                }
+            }
+        });
+    } else {
+        if (typeof user_id === 'undefined')
+            data_to_send = {array: all_choices, product_id: product_id}
+        else
+            data_to_send = {array: all_choices, product_id: product_id, user_id: user_id}
+        $.ajax({
+            type: ajax_post_type,
+            url: ajax_post_url,//post how you get this URL please...
+            data: data_to_send,//jQ will sort this out for you
+            success: function (response) {
+                console.log('aaaaa');
                 if (response.success) {
                     console.log('sucess');
                     window.location.href = response.redirect; // <-- HERE
@@ -253,24 +240,4 @@ $("#post-btn").click(function (e) {
             }
         });
     }
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "/productOptions/list",//post how you get this URL please...
-        data: {array: all_choices, product_id: product_id},//jQ will sort this out for you
-        success: function (response) {
-            console.log('aaaaa');
-            if (response.success) {
-                console.log('sucess');
-                window.location.href = response.redirect; // <-- HERE
-            }
-        },
-        error: function (response) {
-            console.log('an error occured');
-            if (!response.success) {
-                console.log('fail');
-            }
-        }
-    });
 });
-
